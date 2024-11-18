7fd3f7e877a6867587303a0cfc252b2245e7f428
Stan-breaks <stanleymwendwa03@gmail.com>
2024-11-18 17:57:19 +0300

    leak

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,14 +7,17 @@ import { getAnalytics, Analytics } from "firebase/analytics";
 
 // Firebase configuration object
 
+  
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC7rCpHgf2Dzp0guMnhNm-xO21Bm7YUmiQ",
+  authDomain: "eptaevents-e6217.firebaseapp.com",
+  databaseURL: "https://eptaevents-e6217-default-rtdb.firebaseio.com",
+  projectId: "eptaevents-e6217",
+  storageBucket: "eptaevents-e6217.firebasestorage.app",
+  messagingSenderId: "664503244131",
+  appId: "1:664503244131:web:1d0ef8d24fcc94ac5f6f56",
 };
+
 // Initialize Firebase app
 const app: FirebaseApp = initializeApp(firebaseConfig);
 
